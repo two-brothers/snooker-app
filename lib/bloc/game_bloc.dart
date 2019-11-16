901deeb7859e01b0e422630af5944b8e41a43637
nikeshnazareth <business@nikeshnazareth.com>
2019-11-16 16:31:36 +1100

    Use BehaviorSubject in GameBloc

--- lib/bloc/game_bloc.dart
+++ lib/bloc/game_bloc.dart
@@ -1,27 +1,29 @@
-import 'dart:async';
 import 'package:meta/meta.dart';
+import 'package:rxdart/rxdart.dart';
+
+import 'dart:async';
 import 'bloc.dart';
 
 class GameBloc implements Bloc {
-  final _gameController = StreamController<Game>();
+  final _gameController = BehaviorSubject<Game>();
   Game _game;
 
   Stream<Game> get stream => _gameController.stream;
 
   void initPlayers(String player1, String player2) {
     _game = Game(player1: Player(name: player1, score: 0), player2: Player(name: player2, score: 0));
-    _gameController.sink.add(_game);
+    _gameController.add(_game);
   }
 
   void addPoints({@required int playerId, @required int increment}) {
     if (playerId == 0) {
       Player _player = _game.player1;
       _game = Game(player1: Player(name: _player.name, score: _player.score + increment), player2: _game.player2);
-      _gameController.sink.add(_game);
+      _gameController.add(_game);
     } else if (playerId == 1) {
       Player _player = _game.player2;
       _game = Game(player1: _game.player1, player2: Player(name: _player.name, score: _player.score + increment));
-      _gameController.sink.add(_game);
+      _gameController.add(_game);
     }
   }
 
